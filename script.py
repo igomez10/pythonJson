f2c24177cae1166792f788f9fcffa335a90cf7b3
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-03-11 22:11:57 -0500

    1.0
    MVP

--- script.py
+++ script.py
@@ -1,3 +1,47 @@
 import json
 
 print "-----------------  Start script.py --------------- "
+
+
+data = {}
+data['nodes'] = []
+data['nodes'].append({
+    'id': 'Ignacio',
+    'group': 1
+})
+
+data['nodes'].append({
+    'id': 'Alberto',
+    'group': 1
+})
+
+data['nodes'].append({
+    'id': 'Daniel',
+    'group': 1
+})
+
+
+data['links'] = []
+data['links'].append({
+    'source': 'Ignacio',
+    'target': 'Alberto',
+    'value': 1
+})
+
+data['links'].append({
+    'source': 'Ignacio',
+    'target': 'Daniel',
+    'value': 1
+})
+
+data['links'].append({
+    'source': 'Daniel',
+    'target': 'Alberto',
+    'value': 1
+})
+
+
+
+
+with open('file.json', 'w') as outfile:
+    json.dump(data, outfile)
